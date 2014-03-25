7726c222be02f5808b93b05bc2bbfe984b5d7e3e
William Xie <wxie6@illinois.edu>
2014-03-25 02:17:54 -0500

    Work in progress

--- erino.js
+++ erino.js
@@ -8,39 +8,47 @@ function translateModel() {
 
 	// http://www.englishclub.com/grammar/prepositions-list.htm
 	// Prepositions omitted
-	var prepositions = ["aboard", "about", "above", "across", "after", "against",
-						"along", "amid", "among", "anti", "around", "as", "at", "before", 
-						"behind", "below", "beneath", "beside", "besides", "between",
-						"beyond", "but", "by", "despite", "down", "except", "for", "from",
-						"in", "inside", "into", "like", "minus", "near", "of", "off", "on", 
-						"onto", "opposite", "outside", "over", "past", "per", "plus", 
-						"round", "save", "since", "than", "through", "to", "toward", 
-						"towards", "under", "underneath", "unlike", "until", "up", 
-						"upon", "versus", "via", "with", "within", "without"];
+	self.prepositions = ["aboard", "about", "above", "across", "after", "against",
+			"along", "amid", "among", "anti", "around", "as", "at", "before", 
+			"behind", "below", "beneath", "beside", "besides", "between",
+			"beyond", "but", "by", "despite", "down", "except", "for", "from",
+			"in", "inside", "into", "like", "minus", "near", "of", "off", "on", 
+			"onto", "opposite", "outside", "over", "past", "per", "plus", 
+			"round", "save", "since", "than", "through", "to", "toward", 
+			"towards", "under", "underneath", "unlike", "until", "up", 
+			"upon", "versus", "via", "with", "within", "without"];
 
     // http://grammar.about.com/od/words/a/EnglishContractions.htm
 	// Contractions omitted
-	var contractions = ["aren't", "can't", "couldn't", "didn't", "doesn't", "don't",
-						"hadn't", "hasn't", "haven't", "he'd", "he'll", "he's", "I'd",
-						"I'll", "I'm", "I've", "isn't", "it's", "let's", "mightn't",
-						"mustn't", "shan't", "she'd", "she'll", "she's", "shouldn't",
-						"that's", "there's", "they'd", "they'll", "they're", "they've",
-						"we'd", "we're", "we've", "weren't", "what'll", "what're", 
-						"what's", "what've", "where's", "who'd", "who'll", "who're",
-						"who's", "who've", "won't", "wouldn't", "you'd", "you'll",
-						"you're", "you've"];
+	self.contractions = ["aren't", "can't", "couldn't", "didn't", "doesn't", "don't",
+			"hadn't", "hasn't", "haven't", "he'd", "he'll", "he's", "I'd",
+			"I'll", "I'm", "I've", "isn't", "it's", "let's", "mightn't",
+			"mustn't", "shan't", "she'd", "she'll", "she's", "shouldn't",
+			"that's", "there's", "they'd", "they'll", "they're", "they've",
+			"we'd", "we're", "we've", "weren't", "what'll", "what're", 
+			"what's", "what've", "where's", "who'd", "who'll", "who're",
+			"who's", "who've", "won't", "wouldn't", "you'd", "you'll",
+			"you're", "you've"];
 
 	// Articles, pronouns, and other things omitted
-	var articles = ["is", "am", "are", "was", "were", "be", "being", "been", "have", 
-					"had", "having", "me", "he", "him", "she", "her", "it",
-					"the", "this", "these", "those", "there", "their", "than", "then"];
+	self.articles = ["is", "am", "are", "was", "were", "be", "being", "been", "have", 
+			"had", "having", "me", "he", "him", "she", "her", "it",
+			"the", "this", "these", "those", "there", "their", "than", "then"];
 
-	var line = "";
+	self.line = ko.observable('Insert text to translate here');
 
-	self.displayLine = ko.computed(function() {
-		console.log(line);
-		return line;
-	});
+	self.getLine = function() {
+		console.log("GET LINE");
+		return self.line;
+	};
+
+	
+	self.translateLine = function() {
+		self.line("THERE IS NOTHING TO TRANSLATE HERE");
+		console.log(self.line());
+		return self.line;
+	};
+
 }
 
 ko.applyBindings(new translateModel());
